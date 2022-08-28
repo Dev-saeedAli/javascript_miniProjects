1b2caea014cca09319295141184a8bb95d9aff2d
saeed ali <mohammedsaidali69@gmail.com>
2022-08-28 22:46:44 +0400

    menu app styleshet added

---
+++ menu_App/index.js
@@ -0,0 +1,8 @@
+const dishArray = [ 
+    {
+        id: 1,
+        name : "Buttermilk Pancakes",
+        price : 10.00,
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    }
+]and also we ca
